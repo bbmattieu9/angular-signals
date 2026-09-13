ed596e0e37ad3dfca76e19f0a73abbac422d1ac7
Babatunde Mattieu <bbmattieu9@gmail.com>
2026-09-13 23:36:24 +0100

    feat: auth - setup auth in login template, login class component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,15 +5,41 @@ import {MessagesService} from "../messages/messages.service";
 import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
-    selector: 'login',
-    imports: [
-        RouterLink,
-        ReactiveFormsModule
-    ],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss'
+  selector: 'login',
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  messagesSrv = inject(MessagesService);
 
+  fb = inject(FormBuilder);
+  form = this.fb.group({
+    email: [''],
+    password: ['']
+  })
+
+  onLogin() {
+    try {
+      const {email, password} = this.form.value;
+       if (!email || !password) {
+         this.messagesSrv.showMessage(
+           "Enter an email and password",
+           "info"
+         )
+       }
+
+    }
+    catch (err) {
+      console.log(err);
+      this.messagesSrv.showMessage(
+        "Loagin Failed, Please try again later.",
+        "error"
+      )
+    }
+  }
 }
