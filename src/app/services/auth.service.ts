888faaf680fb80680a91754fb6750ba442d3b5f7
Babatunde Mattieu <bbmattieu9@gmail.com>
2026-09-14 22:08:29 +0100

    feat: auth -Add AuthService - integrate with the home component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import {computed, effect, inject, Injectable, signal} from "@angular/core";
 import {User} from "../models/user.model";
 import {environment} from "../../environments/environment";
 import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom} from "rxjs";
 
 const USER_STORAGE_KEY = 'user';
 
@@ -9,6 +11,21 @@ const USER_STORAGE_KEY = 'user';
   providedIn: 'root'
 })
 export class AuthService {
+
+  httpMessenger = inject(HttpClient);
+  #userSignal = signal<User | null>(null);
+  user = this.#userSignal.asReadonly();
+  isLoggedIn = computed(() => !!this.user());
+
+  async login(email: string, password: string): Promise<User> {
+    const login$ = this.httpMessenger.post<User>(`${environment.apiRoot}/login`, {
+      email,
+      password
+    });
+    const user = await firstValueFrom(login$);
+    this.#userSignal.set(user);
+    return user;
+  }
 
 
 }
